5da081f3b47b73f1a857b80e4a8cd55387f67bd5
Daniel Oy <roi@nasa.com.mx>
2021-02-28 20:41:38 -0600

    Initial code added

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                HelloWorldConsole.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(result, "Hello World!");
+            }
+        }
+    }
+}
